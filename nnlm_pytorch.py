51093d1ce7c3173392fc9313d1123602e3c116f1
joonyeongs <sjoonyeong@gmail.com>
2023-11-02 17:36:19 +0900

    Load dataset Wikitext2 and tokenize/batchify

--- nnlm_pytorch.py
+++ nnlm_pytorch.py
@@ -6,23 +6,38 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchtext.datasets import WikiText2
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from torch import nn, Tensor
+from torch.utils.data import dataset
 
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "cpu"
-)
 
+
+def tokenize_and_batch(raw_text_iter: dataset.IterableDataset, batch_size) -> Tensor:
+    # Tokenize and convert raw text into a list of tensors,
+    # where each tensor represents a sequence of tokens
+    data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
+    # Eliminate empty tensors and concatenate into a single tensor
+    data = torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
+
+    num_batches = data.shape[0] // batch_size 
+    data = data[:num_batches * batch_size]                       
+    data = data.view(batch_size, num_batches)   
+           
+    return data
+
+def get_batch(data, seq_len, idx):
+    src = data[:, idx:idx+seq_len]                   
+    target = data[:, idx+1:idx+seq_len+1]             
+    return src, target
+
+                     
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
+            
         )
 
     def forward(self, x):
@@ -30,11 +45,19 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
     
-if __name__ == "__main__":
-    #model = NeuralNetwork().to(device)
-    #print(model)
-    train_set, valid_set, test_set = WikiText2()
 
-    for i, text in enumerate(train_set):
-        if i == 5: break
-        print(f'[{i}]: {text}')
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    data_for_dict = WikiText2(split='train')    
+    tokenizer = get_tokenizer('basic_english')
+    vocab = build_vocab_from_iterator(map(tokenizer, data_for_dict), specials=['<unk>'])
+    vocab.set_default_index(vocab['<unk>'])
+
+    
+    train_iter, val_iter, test_iter = WikiText2()
+    train_data = tokenize_and_batch(train_iter, batch_size=32)
+    val_data = tokenize_and_batch(val_iter, batch_size=32)
+    test_data = tokenize_and_batch(test_iter, batch_size=32)
+    
+    
