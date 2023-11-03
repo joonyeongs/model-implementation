fbe760417149ff9dac2fdb1dd567fc2224c98610
joonyeongs <sjoonyeong@gmail.com>
2023-11-03 14:04:15 +0900

    Add dataloader, create nnlm, implement training

--- nnlm_pytorch.py
+++ nnlm_pytorch.py
@@ -9,55 +9,108 @@ from torchtext.datasets import WikiText2
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torch import nn, Tensor
-from torch.utils.data import dataset
+from torch.utils.data import TensorDataset, DataLoader, dataset
 
 
 
-def tokenize_and_batch(raw_text_iter: dataset.IterableDataset, batch_size) -> Tensor:
-    # Tokenize and convert raw text into a list of tensors,
-    # where each tensor represents a sequence of tokens
+def tokenize_and_split(raw_text_iter: dataset.IterableDataset, total_samples: int) -> Tensor:
+    """
+    1. Tokenize and convert raw text into a list of tensors, where each tensor represents a sequence of tokens
+    2. Eliminate empty tensors and concatenate into a single tensor
+    3. Split the single tensor into samples, where each sample represents a sequence of tokens
+    """
+    
     data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
-    # Eliminate empty tensors and concatenate into a single tensor
     data = torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
 
-    num_batches = data.shape[0] // batch_size 
-    data = data[:num_batches * batch_size]                       
-    data = data.view(batch_size, num_batches)   
+    length_of_sequence = data.shape[0] // total_samples 
+    data = data[:length_of_sequence * total_samples]                       
+    data = data.view(total_samples, length_of_sequence)   
            
     return data
 
-def get_batch(data, seq_len, idx):
-    src = data[:, idx:idx+seq_len]                   
-    target = data[:, idx+1:idx+seq_len+1]             
-    return src, target
+def create_batch_from_dataloader(text_data_tensor: Tensor, batch_size: int) -> DataLoader:
+    dataset = TensorDataset(text_data_tensor)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    return dataloader 
 
                      
 class NeuralNetwork(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            
-        )
+        super(NeuralNetwork).__init__()
+        """
+        y = b + Wx + U * tanh(d + Hx)
+        """
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.h = nn.Linear(n_steps * embedding_dim, n_hidden, bias=False)
+        self.d = nn.Parameter(torch.ones(n_hidden))
+        self.tanh = nn.Tanh()
+        self.u = nn.Linear(n_hidden, vocab_size, bias=False)
+        self.w = nn.Linear(n_steps * embedding_dim, vocab_size, bias=False)
+        self.b = nn.Parameter(torch.ones(vocab_size))
+        self.softmax = nn.Softmax()
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        x = self.embedding(x)
+        x = x.view(-1, n_steps * embedding_dim)     # need to check this
+        tanh = self.tanh(self.d + self.h(x))
+        y = self.b + self.w(x) + self.u(tanh)
+        return y
     
 
 if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    embedding_dim = 1000
+    total_samples = 10000
+    n_steps = 5
+    n_hidden = 10
+    batch_size = 64
 
     data_for_dict = WikiText2(split='train')    
     tokenizer = get_tokenizer('basic_english')
     vocab = build_vocab_from_iterator(map(tokenizer, data_for_dict), specials=['<unk>'])
     vocab.set_default_index(vocab['<unk>'])
-
+    vocab_size = len(vocab)
     
     train_iter, val_iter, test_iter = WikiText2()
-    train_data = tokenize_and_batch(train_iter, batch_size=32)
-    val_data = tokenize_and_batch(val_iter, batch_size=32)
-    test_data = tokenize_and_batch(test_iter, batch_size=32)
+    train_data = tokenize_and_split(train_iter, total_samples)
+    #val_data = tokenize_and_split(val_iter, total_samples)
+    #test_data = tokenize_and_split(test_iter, total_samples)
+    train_dataloader = create_batch_from_dataloader(train_data, batch_size)
+    '''
+    Check the output of the dataloader
+
+    for batch in train_dataloader:
+        print(batch[0][:, -1])
+        print(batch[0][:, -1].view(-1, 1))
+        break
+    '''
+    # Training
+    model = NeuralNetwork().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    for epoch in range(1000):
+        for batch in train_dataloader:
+            input_batch, target_batch = batch[0][:, :-1], batch[0][:, -1]
+            input_batch = input_batch.to(device)
+            target_batch = target_batch.to(device)
+
+            optimizer.zero_grad()
+            output = model(input_batch)
+            loss = criterion(output, target_batch)
+            loss.backward()
+            optimizer.step()
+
+        if (epoch + 1) % 1000 == 0:
+            print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+
+    # Predict
+
+    # Test
+    
+
+        
     
     
